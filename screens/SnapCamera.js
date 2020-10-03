a80449510f4573f76b8d02e36417ddf545da2ca9
Daniel Lujan <daniel.lujan96@gmail.com>
2020-10-02 20:00:32 -0500

    Add SnapCamera screen

---
+++ screens/SnapCamera.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function SnapCamera() {
+  return (
+    <View style={styles.container}>
+      <Text>Camera</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
