9b9918e7e168f293bd9d46c401e73ed38d219831
Daniel Lujan <daniel.lujan96@gmail.com>
2020-11-23 21:01:11 -0600

    Store Dropbox token using AsyncStorage, make sure it dont request a token if one already exists, and get the token inside Templates component if it exists

--- screens/Templates.js
+++ screens/Templates.js
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Templates() {
+  
+  const createTemplate = () => {
+    getDBToken();
+  }
+
+  const getDBToken = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@storage_Key')
+      if(value !== null) {
+        console.log(`Get token inside Templates: ${value}`);
+      }
+    } catch(e) {
+      console.error(e);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Templates</Text>
       <Text>Template 1</Text>
       <Text>Template 2</Text>
+      <Button title="Create Template" onPress={() => createTemplate()}/>
       <StatusBar style="auto" />
     </View>
   );
