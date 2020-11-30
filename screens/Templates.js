0ed59ee31b35b875b7b0ded2b6eecc4a1b7d6058
Daniel Lujan <daniel.lujan96@gmail.com>
2020-11-29 20:16:42 -0600

    Create button in Templates that adds a static test template to user album_templates

--- screens/Templates.js
+++ screens/Templates.js
@@ -1,19 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import firebase from 'firebase';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Templates() {
   
-  const createTemplate = () => {
-    getDBToken();
+  const createTemplate = async () => {
+    // Create a template format and save to database
+    const uid = await getUID();
+    let ref = firebase.database().ref('/users/' + uid);
+
+    // **** I need to figure out how to be able to create deeper folders with Firebase, it won't take an array :/
+    // Using a static test template for now, but will eventually be a dynamic one (aka object) that user creates
+    let testTemplate = {
+      title: 'test-template',
+      foundation: 'foundation',
+      roof: 'roof',
+      plumbing: 'plumbing'
+    }
+
+    ref.child("album_templates").push(testTemplate);
   }
 
   const getDBToken = async () => {
     try {
       const value = await AsyncStorage.getItem('@storage_Key')
       if(value !== null) {
-        console.log(`Get token inside Templates: ${value}`);
+        return value;
+      }
+    } catch(e) {
+      console.error(e);
+    }
+  }
+
+  const getUID = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@user_Id')
+      if(value !== null) {
+        return value;
       }
     } catch(e) {
       console.error(e);
