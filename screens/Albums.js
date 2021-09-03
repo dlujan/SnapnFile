f943769d4d3cd9f7e1c0693a1db7cbbe638ddddc
Daniel Lujan <daniel.lujan96@gmail.com>
2021-09-02 19:24:11 -0500

    New album change forward slashes to hyphens

--- screens/Albums.js
+++ screens/Albums.js
@@ -143,15 +143,16 @@ class Albums extends React.Component {
 
     if (this.state.newAlbumName !== '' && this.state.newAlbumName.trim() !== "" && this.state.newAlbumTemplate !== null) {
 
-      const testAlbum = {
+      let newAlbum = {
         id: Date.now(),
         name: this.state.newAlbumName,
         template: this.state.allTemplates[this.state.newAlbumTemplate]
       };
+      newAlbum.name = newAlbum.name.replace(/\//g, '-');
   
       // Create temporary copy of all albums in state and push new album to it
       let allAlbumsToSave = this.state.allAlbums;
-      allAlbumsToSave.push(testAlbum);
+      allAlbumsToSave.push(newAlbum);
   
       try {
         // Set state's allAlbums into storage, including the new one
